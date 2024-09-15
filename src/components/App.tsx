b001a03e4913159e0fff09fdfcac7c6246c90c72
Brendan Dagys <brendandagys@gmail.com>
2024-09-15 18:46:25 -0400

    More refactoring of initial files

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import "../css/App.css";
+
+function App() {
+  return (
+    <>
+      <h1>{"Hello, I'm Brendan."}</h1>
+      <h2 className="text-gray">
+        {"I'm a software developer and AWS architect."}{" "}
+      </h2>
+      <button className="button mt-5">View my work</button>
+    </>
+  );
+}
+
+export default App;
