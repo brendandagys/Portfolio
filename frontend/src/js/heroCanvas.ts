067f98d2201b268d87ee49330c2028855fb85bf3
Brendan Dagys <brendandagys@gmail.com>
2024-09-24 20:14:46 -0400

    Refactor hero animation

--- frontend/src/js/heroCanvas.ts
+++ frontend/src/js/heroCanvas.ts
@@ -1,104 +1,180 @@
-const canvasDots = function () {
-  const canvas = document.querySelector('canvas'),
-    ctx = canvas.getContext('2d'),
-    colorDot = [
-      'rgb(81, 162, 233)',
-      'rgb(81, 162, 233)',
-      'rgb(81, 162, 233)',
-      'rgb(81, 162, 233)',
-      'rgb(255, 77, 90)',
-    ], // 80% of dots are blue. 20% pink
-    color = 'rgb(81, 162, 233)';
+import { getDotsConfiguration } from "./heroDotsConfiguration";
 
-  // ctx.globalAlpha = 0.8;
-  canvas.width = document.body.scrollWidth;
+let canvas: HTMLCanvasElement | null;
+let ctx: CanvasRenderingContext2D | null;
+let dotsAnimation: DotsAnimation;      // Custom class
+let animation: number;  // Handler returned by `requestAnimationFrame(callback)`
+
+const mousePosition = { x: 0, y: 0 };
+
+export const heroAnimation = () => {
+  canvas = document.querySelector('.canvas-1');
+  if (canvas === null) return;
+  ctx = canvas.getContext('2d');
+  if (ctx === null) return;
+
+  canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   canvas.style.display = 'block';
-  // ctx.fillStyle = colorDot;
-  // ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 50%, 50%)';
+
   ctx.lineWidth = 0.3;
-  ctx.strokeStyle = color;
+  ctx.strokeStyle = 'rgb(81, 162, 233)';
 
-  let mousePosition = {
-    x: (30 * canvas.width) / 100,
-    y: (30 * canvas.height) / 100,
-  };
+  dotsAnimation = new DotsAnimation(ctx, canvas.width, canvas.height);
 
-  const windowSize = window.innerWidth;
-  let dots;
+  window.addEventListener("mousemove", function (e) {
+    mousePosition.x = e.clientX;
+    mousePosition.y = e.clientY;
 
-  if (windowSize > 1600) {
-    dots = {
-      nb: 600, // number of particles
-      distance: 70, // max distance between particles for them to link
-      d_radius: 300, // radius from mouse location that particles will link
-      array: [],
-    };
-  } else if (windowSize > 1300) {
-    dots = {
-      nb: 575,
-      distance: 60,
-      d_radius: 280,
-      array: [],
-    };
-  } else if (windowSize > 1100) {
-    dots = {
-      nb: 500,
-      distance: 55,
-      d_radius: 250,
-      array: [],
-    };
-  } else if (windowSize > 800) {
-    dots = {
-      nb: 300,
-      distance: 0,
-      d_radius: 0,
-      array: [],
-    };
-  } else if (windowSize > 600) {
-    dots = {
-      nb: 200,
-      distance: 0,
-      d_radius: 0,
-      array: [],
-    };
-  } else {
-    dots = {
-      nb: 100,
-      distance: 0,
-      d_radius: 0,
-      array: [],
-    };
+    dotsAnimation.dots[0].x = e.clientX;
+    dotsAnimation.dots[0].y = e.clientY;
+  });
+
+  dotsAnimation.animate(0);
+};
+
+window.addEventListener("resize", function () {
+  cancelAnimationFrame(animation);
+
+  if (!canvas || !ctx) return;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  dotsAnimation = new DotsAnimation(ctx, canvas.width, canvas.height);
+  dotsAnimation.animate(0);
+});
+
+class DotsAnimation {
+  #ctx;
+  #width;
+  #height;
+  #dotsConfiguration;
+  dots: Dot[] = [];
+  lastTimestamp: number;
+  timer: number;
+  interval: number;
+
+  constructor(ctx: CanvasRenderingContext2D, width: number, height: number) {
+    this.#ctx = ctx;
+    this.#width = width;
+    this.#height = height;
+    this.#dotsConfiguration = getDotsConfiguration();
+    this.lastTimestamp = 0;
+    this.timer = 0;
+    this.interval = 1000 / 60;
+
+    this.createDots();
   }
 
-  // decided to turn off connecting dots under 1100px
+  createDots() {
+    this.dots = [];
 
-  // } else if (windowSize > 650) {
-  //   dots = {
-  //     nb: 400,
-  //     distance: 50,
-  //     d_radius: 185,
-  //     array: [],
-  //   };
-  // } else if (windowSize > 500) {
-  //   dots = {
-  //     nb: 325,
-  //     distance: 45,
-  //     d_radius: 170,
-  //     array: [],
-  //   };
-  // } else {
-  //   dots = {
-  //     nb: 270,
-  //     distance: 45,
-  //     d_radius: 140,
-  //     array: [],
-  //   };
-  // }
+    for (let i = 0; i < this.#dotsConfiguration.nb; i++) {
+      this.dots.push((new Dot(this.#ctx, this.#width, this.#height)).paint());
+    }
+  }
 
-  function Dot() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
+  calculateLines() {
+    for (let i = 0; i < this.dots.length; i++) {
+      const dot1 = this.dots[i];
+      dot1.paint();
+
+      for (let j = i + 1; j < this.dots.length; j++) {
+        const dot2 = this.dots[j];
+
+        if (
+          dot1.x - dot2.x < this.#dotsConfiguration.distance &&
+          dot1.y - dot2.y < this.#dotsConfiguration.distance &&
+          dot1.x - dot2.x > -this.#dotsConfiguration.distance &&
+          dot1.y - dot2.y > -this.#dotsConfiguration.distance
+        ) {
+          if (
+            dot1.x - mousePosition.x < this.#dotsConfiguration.d_radius &&
+            dot1.y - mousePosition.y < this.#dotsConfiguration.d_radius &&
+            dot1.x - mousePosition.x > -this.#dotsConfiguration.d_radius &&
+            dot1.y - mousePosition.y > -this.#dotsConfiguration.d_radius
+          ) {
+            this.#ctx.beginPath();
+            this.#ctx.moveTo(dot1.x, dot1.y);
+            this.#ctx.lineTo(dot2.x, dot2.y);
+
+            // Increase fill color fade the further you are from the mouse
+            const mp = mousePosition;
+
+            let distanceRatio = (
+              (((dot1.x - mp.x) ** 2 + (dot1.y - mp.y) ** 2) ** 0.5)  // Dot distance from mouse
+              /
+              this.#dotsConfiguration.d_radius
+            );
+
+            // Prevent it from fading out completely
+            distanceRatio = Math.max(distanceRatio - 0.3, 0);
+
+            this.#ctx.strokeStyle = `rgb(81, 162, 233, ${1 - distanceRatio})`;  // eslint-disable-line @typescript-eslint/restrict-template-expressions
+
+            this.#ctx.stroke();
+            this.#ctx.closePath();
+          }
+        }
+      }
+    }
+  }
+
+  setNextDotPositions() {
+    // Don't animate the first dot; it will follow mouse
+    for (const dot of this.dots.slice(1)) {
+      if (dot.y < 0 || dot.y > this.#height) dot.vy *= -1;
+      else if (dot.x < 0 || dot.x > this.#width) dot.vx *= -1;
+
+      dot.x += dot.vx;
+      dot.y += dot.vy;
+    }
+  }
+
+  animate(timestamp: number) {
+    const deltaTime = timestamp - this.lastTimestamp;
+    this.lastTimestamp = timestamp;
+    if (this.timer > this.interval) {
+      this.#ctx.clearRect(0, 0, this.#width, this.#height);
+
+      this.calculateLines();
+      this.setNextDotPositions();
+
+      this.timer = 0;
+    } else {
+      this.timer += deltaTime;
+    }
+
+    animation = requestAnimationFrame(this.animate.bind(this));
+  }
+}
+
+const colorDot = [
+  'rgb(81, 162, 233)',
+  'rgb(81, 162, 233)',
+  'rgb(81, 162, 233)',
+  'rgb(81, 162, 233)',
+  'rgb(255, 77, 90)',
+];
+
+class Dot {
+  ctx: CanvasRenderingContext2D;
+  x: number;
+  y: number;
+  vx: number;
+  vy: number;
+  radius: number;
+  colour: string;
+  canvasWidth: number;
+  canvasHeight: number;
+
+  constructor(ctx: CanvasRenderingContext2D, canvasWidth: number, canvasHeight: number) {
+    this.ctx = ctx;
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+
+    this.x = Math.random() * canvasWidth;
+    this.y = Math.random() * canvasHeight;
 
     this.vx = -0.5 + Math.random();
     this.vy = -0.5 + Math.random();
@@ -109,129 +185,22 @@ const canvasDots = function () {
     this.colour = colorDot[Math.floor(Math.random() * colorDot.length)];
   }
 
-  Dot.prototype = {
-    create: function () {
-      ctx.beginPath();
-      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      // ctx.fillStyle = this.colour;
+  paint(): this {
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
 
-      // make the dot colour fade out the further they are from the mouse
-      const dotDistance =
-        ((this.x - mousePosition.x) ** 2 + (this.y - mousePosition.y) ** 2) **
-        0.5;
-      const distanceRatio = dotDistance / (windowSize / 1.7);
+    const top = window.scrollY;
 
-      // this chops the bracket off the rgb colour and ads an opacity
-      ctx.fillStyle = this.colour.slice(0, -1) + `,${1 - distanceRatio})`;
+    // make the dot colour fade out the further they are from the mouse
+    const dotDistance = (
+      ((this.x - mousePosition.x) ** 2 + (this.y - mousePosition.y + top) ** 2) ** 0.5
+    );
+    const distanceRatio = dotDistance / (window.innerWidth / 1.7);
 
-      ctx.fill();
-    },
+    // this chops the bracket off the rgb colour and ads an opacity
+    this.ctx.fillStyle = this.colour.slice(0, -1) + `,${1 - distanceRatio})`;  // eslint-disable-line @typescript-eslint/restrict-template-expressions
+    this.ctx.fill();
 
-    animate: function () {
-      // dont animate the first dot, it will follow mouse
-      for (let i = 1; i < dots.nb; i++) {
-        const dot = dots.array[i];
-
-        if (dot.y < 0 || dot.y > canvas.height) {
-          dot.vx = dot.vx;
-          dot.vy = -dot.vy;
-        } else if (dot.x < 0 || dot.x > canvas.width) {
-          dot.vx = -dot.vx;
-          dot.vy = dot.vy;
-        }
-        dot.x += dot.vx;
-        dot.y += dot.vy;
-      }
-    },
-
-    line: function () {
-      for (let i = 0; i < dots.nb; i++) {
-        for (let j = 0; j < dots.nb; j++) {
-          const i_dot = dots.array[i];
-          const j_dot = dots.array[j];
-
-          if (
-            i_dot.x - j_dot.x < dots.distance &&
-            i_dot.y - j_dot.y < dots.distance &&
-            i_dot.x - j_dot.x > -dots.distance &&
-            i_dot.y - j_dot.y > -dots.distance
-          ) {
-            if (
-              i_dot.x - mousePosition.x < dots.d_radius &&
-              i_dot.y - mousePosition.y < dots.d_radius &&
-              i_dot.x - mousePosition.x > -dots.d_radius &&
-              i_dot.y - mousePosition.y > -dots.d_radius
-            ) {
-              ctx.beginPath();
-              ctx.moveTo(i_dot.x, i_dot.y);
-              ctx.lineTo(j_dot.x, j_dot.y);
-
-              // make the fill colour fade out the further you are from the mouse
-              const dotDistance =
-                ((i_dot.x - mousePosition.x) ** 2 +
-                  (i_dot.y - mousePosition.y) ** 2) **
-                0.5;
-              let distanceRatio = dotDistance / dots.d_radius;
-
-              // make it so it doesnt fade out completely
-              distanceRatio -= 0.3;
-              if (distanceRatio < 0) {
-                distanceRatio = 0;
-              }
-
-              ctx.strokeStyle = `rgb(81, 162, 233, ${1 - distanceRatio})`;
-
-              ctx.stroke();
-              ctx.closePath();
-            }
-          }
-        }
-      }
-    },
-  };
-
-  function createDots() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < dots.nb; i++) {
-      dots.array.push(new Dot());
-      var dot = dots.array[i];
-
-      dot.create();
-    }
-
-    // first dot to be relativley large
-    dots.array[0].radius = 1.5;
-
-    // first dot to be blue
-    dots.array[0].colour = '#51a2e9';
-    dot.line();
-    dot.animate();
+    return this;
   }
-
-  window.onmousemove = function (parameter) {
-    mousePosition.x = parameter.pageX;
-    mousePosition.y = parameter.pageY;
-
-    // sometimes if the mouse is off screen on refresh, it bugs out
-    try {
-      // want the first dot to follow the mouse
-      dots.array[0].x = parameter.pageX;
-      dots.array[0].y = parameter.pageY;
-    } catch {
-      //
-    }
-  };
-
-  mousePosition.x = window.innerWidth / 2;
-  mousePosition.y = window.innerHeight / 2;
-
-  const draw = setInterval(createDots, 1000 / 30);
-
-  window.onresize = function () {
-    clearInterval(draw);
-    canvasDots();
-    // console.log(yippe);
-  };
-};
-
-export default canvasDots;
+}
---
+++ frontend/src/js/heroDotsConfiguration.ts
@@ -0,0 +1,74 @@
+export interface DotsConfiguration {
+  nb: number;
+  distance: number;
+  d_radius: number;
+}
+
+const dotsConfiguration: Record<number, DotsConfiguration> = {
+  1600: {          // Minimum width
+    nb: 600,       // Number of dots
+    distance: 70,  // Maximum distance between dots to link them
+    d_radius: 300, // Radius from mouse location within which dots will link
+  },
+  1300: {
+    nb: 575,
+    distance: 60,
+    d_radius: 280,
+  },
+  1100: {
+    nb: 500,
+    distance: 55,
+    d_radius: 250,
+  },
+  800: {
+    nb: 300,
+    distance: 0,
+    d_radius: 0,
+  },
+  600: {
+    nb: 200,
+    distance: 0,
+    d_radius: 0,
+  },
+  0: {
+    nb: 100,
+    distance: 0,
+    d_radius: 0,
+  }
+};
+
+export const getDotsConfiguration = (): DotsConfiguration => {
+  return {
+    ...dotsConfiguration[
+    Object.keys(dotsConfiguration)
+      .map(Number)
+      .sort((a, b) => b - a) // Sort descending; numeric keys are returned in increasing order
+      .find((width) => window.innerWidth >= width) ?? 0
+    ],
+  };
+};
+
+// decided to turn off connecting dots under 1100px
+
+// } else if (window.innerWidth > 650) {
+//   dots = {
+//     nb: 400,
+//     distance: 50,
+//     d_radius: 185,
+//     array: [],
+//   };
+// } else if (window.innerWidth > 500) {
+//   dots = {
+//     nb: 325,
+//     distance: 45,
+//     d_radius: 170,
+//     array: [],
+//   };
+// } else {
+//   dots = {
+//     nb: 270,
+//     distance: 45,
+//     d_radius: 140,
+//     array: [],
+//   };
+// }
