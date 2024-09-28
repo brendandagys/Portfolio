ba8d6ffe1c1df2b07956c56646e1edab38670fa8
Brendan Dagys <brendandagys@gmail.com>
2024-09-28 18:11:27 -0400

    Add animation to About section text

--- frontend/src/js/contentAnimations.ts
+++ frontend/src/js/contentAnimations.ts
@@ -1,26 +1,3 @@
-function techIconsFadeIn(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      icons.forEach((icon, i) => {
-        const iconElement = document.querySelector<HTMLImageElement>(`.${icon}`);
-
-        if (iconElement) {
-          if (spinIcons.includes(icon)) {
-            iconElement.style.animationName = 'slow-bob-spin, fade-in';
-            if (icon === "icon-k8") iconElement.style.animationDirection = "reverse, normal";
-            iconElement.style.animationDelay = `0s, ${i * .04}s`;
-          } else {
-            iconElement.style.animationName = 'slow-bobbing, fade-in';
-            iconElement.style.animationDelay = `${Math.random() * 3}s, ${i * .04}s`;
-          }
-        }
-      });
-
-      observer.unobserve(entry.target);
-    }
-  });
-}
-
 const icons = [
   "icon-git",
   "icon-javascript",
@@ -46,9 +23,62 @@ const icons = [
 
 const spinIcons = ["icon-k8", "icon-react", "icon-rust"];
 
-export const initializeContentAnimations = () => {
-  const observer = new IntersectionObserver(techIconsFadeIn, { root: null, rootMargin: '0px', threshold: 0.5 });
+function techIconsFadeIn(
+  entries: IntersectionObserverEntry[],
+  observer: IntersectionObserver
+) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      icons.forEach((icon, i) => {
+        const iconElement =
+          document.querySelector<HTMLImageElement>(`.${icon}`);
 
+        if (iconElement) {
+          if (spinIcons.includes(icon)) {
+            iconElement.style.animationName = 'slow-bob-spin, fade-in';
+            if (icon === "icon-k8")
+              iconElement.style.animationDirection = "reverse, normal";
+            iconElement.style.animationDelay = `0s, ${0.7 + i * .04}s`;
+          } else {
+            iconElement.style.animationName = 'slow-bobbing, fade-in';
+            iconElement.style.animationDelay =
+              `${Math.random() * 3}s, ${0.7 + i * .04}s`;
+          }
+        }
+      });
+
+      observer.unobserve(entry.target);
+    }
+  });
+}
+
+function aboutMeTextSlideInFromLeft(
+  entries: IntersectionObserverEntry[],
+  observer: IntersectionObserver
+) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const aboutCard = document.querySelector<HTMLDivElement>('.about-card');
+      if (aboutCard) {
+        aboutCard.style.animationName = 'slide-from-left';
+        aboutCard.style.animationDelay = '0s';
+      }
+
+      observer.unobserve(entry.target);
+    }
+  });
+}
+
+export const initializeContentAnimations = () => {
   const aboutSection = document.querySelector("#about");
-  if (aboutSection) observer.observe(aboutSection);
+  if (aboutSection) {
+    (new IntersectionObserver(
+      aboutMeTextSlideInFromLeft,
+      { root: null, rootMargin: '0px', threshold: 0.5 })
+    ).observe(aboutSection);
+
+    (new IntersectionObserver(
+      techIconsFadeIn, { root: null, rootMargin: '0px', threshold: 0.5 })
+    ).observe(aboutSection);
+  }
 };
