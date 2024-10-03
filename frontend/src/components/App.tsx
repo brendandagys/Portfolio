b5de17a85a547c2c0ce7e615bf93e5860e9b86ea
Brendan Dagys <brendandagys@gmail.com>
2024-10-03 19:56:27 -0400

    Refactor Hero into its own component

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,24 +1,18 @@
 import "../css/App.css";
 import "../css/Animations.css";
+import { dotsAnimation } from "../js/dotsAnimation.ts";
 import { initializeContentAnimations } from "../js/contentAnimations.ts";
 
-import { dotsAnimation } from "../js/dotsAnimation.ts";
-import { ContactForm } from "./ContactForm.tsx";
 import { AboutSection } from "./AboutSection.tsx";
-import { WorkSection } from "./WorkSection.tsx";
+import { ContactForm } from "./ContactForm.tsx";
 import { Footer } from "./footer/Footer.tsx";
-import Navbar from "./NavBar.tsx";
+import { Hero } from "./Hero.tsx";
+import { Navbar } from "./NavBar.tsx";
+import { WorkSection } from "./WorkSection.tsx";
 
-import { useScrollTo } from "../hooks/useScrollTo.tsx";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export const App: React.FC = () => {
-  const [configs] = useState({
-    configs: [{ targetId: "work", offset: -100 }],
-  });
-
-  const scrollTo = useScrollTo(configs);
-
   useEffect(() => {
     dotsAnimation(".canvas-1", true);
     dotsAnimation(".canvas-2", false);
@@ -31,22 +25,7 @@ export const App: React.FC = () => {
 
       <div id="home" className="hero">
         <canvas className="canvas-1"></canvas>
-
-        <div className="hero-content">
-          <h1>{"Hello, I'm Brendan."}</h1>
-          <h2 className="text-gray--dark mt-2">
-            {"I'm a software engineer and AWS architect."}
-          </h2>
-          <a className="button-container">
-            {/* prettier-ignore */}
-            <span
-              onClick={() => { scrollTo("work"); }}
-              className="button-span mt-4"
-            >
-              <span className="button-span-inner">See my work</span>
-            </span>
-          </a>
-        </div>
+        <Hero />
       </div>
 
       <div className="main-bg">
