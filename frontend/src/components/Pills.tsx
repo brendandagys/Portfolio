03b1fc1907fb481e8a666bc674c5390b332b2143
Brendan Dagys <bdagys@rentsync.com>
2025-01-13 16:20:27 -0500

    Add title prop to `Pills` component, with default

--- frontend/src/components/Pills.tsx
+++ frontend/src/components/Pills.tsx
@@ -2,12 +2,16 @@ import "../css/Pills.css";
 
 interface PillsProps {
   content: string[];
+  title?: string;
 }
 
-export const Pills: React.FC<PillsProps> = ({ content }) => {
+export const Pills: React.FC<PillsProps> = (
+  { content, title = "Technologies used:" }
+) => {
   return (
     <>
       <div className="pills-container">
+        {title ? <div className="pills-title">{title}</div> : null}
         {content.map((pill) => (
           <span key={pill} className="pill">
             {pill}
